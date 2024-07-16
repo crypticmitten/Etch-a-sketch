38fe4adcdf77c4be75e262ab5c13de276d041802
Isaac Bailey <isaacroybailey@icloud.com>
2024-07-16 15:32:40 +0100

    Squares change on hover

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 
 
-function createGrid(){
+function createGrid(size){
     const grid = document.getElementById("grid");
-    console.log(grid);
-    for(i=0;i<16*16;i++){
+    for(i=0;i<size*size;i++){
         let square = document.createElement("div");
         square.classList.add("square");
+        square.style.width = 800/size;
+        square.style.height = 800/size;
         grid.appendChild(square);
     }
 }
-window.onload = createGrid();
+window.onload = createGrid(16);
+document.addEventListener("mousemove",checkHover);
+
+function checkHover(square){
+    square = document.querySelectorAll(":hover")[3];
+    console.log(square)
+    if (square.classList.contains("square")){
+        square.style.backgroundColor = "red";
+    }
+}
